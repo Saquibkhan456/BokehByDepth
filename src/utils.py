203b9ff6cf2f61c2ff43d5d2ea54c82cbb291139
SaquibKhan <saquibkhan456@gmail.com>
2024-09-10 09:35:29 +0200

    gaussian patch function

--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,6 @@ import torch
 
 
 def split_into_patches(image, num_patches_height=10, num_patches_width=10):
-    
-    # Get image dimensions
     image = torch.from_numpy(image)
     H, W, C = image.shape
     patch_height = H//num_patches_height
@@ -14,7 +12,22 @@ def split_into_patches(image, num_patches_height=10, num_patches_width=10):
     patches = image.unfold(0, patch_height, patch_height).unfold(1, patch_width, patch_width)
     patches = patches.reshape(-1, C, patch_height, patch_width)
     return patches.numpy().transpose(0,2,3,1)
+     
+         
+def gaussian_patch(kernel_size, sigma_patch):
+    """Generates a Gaussian kernel."""
+    h,w = sigma_patch.shape
+    ax = np.arange(-kernel_size // 2 + 1., kernel_size // 2 + 1.)
+    xx, yy = np.meshgrid(ax, ax)
+    term = np.exp(-(xx**2 + yy**2)/2).reshape(-1)
+    term_tiled = np.tile(term, (h, w, 1))
+    gaussian_patch = term_tiled * sigma_patch[:, :, np.newaxis]
+    #kernel = np.exp(term/ (2. * sigma**2))
+    return gaussian_patch
 
+def patch_blurring(image_patch, gaussian_patch):
+    result = np.sum(image_patch*gaussian_patch, axis=-1)
+    result
 
 
 def depth_of_field_effect(image, depthmap, focal_distance, blur_amount):
@@ -32,3 +45,7 @@ def depth_of_field_effect(image, depthmap, focal_distance, blur_amount):
     sharp_image = image * (1-weight_map) + blurred_image * weight_map
     return sharp_image
 
+if __name__ == "__main__":
+    kernel_size = 5
+    sigma_patch = np.random.rand(100,100)
+    gaussian_patch(kernel_size, sigma_patch)
