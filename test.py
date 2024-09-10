7d42621e6b19b1bb76574d81e365f204eb88e30e
SaquibKhan <saquibkhan456@gmail.com>
2024-09-10 22:01:38 +0200

    bug fix

--- test.py
+++ test.py
@@ -8,19 +8,19 @@ from src.utils import *
 def bokeh_effect(image, focal_distance, kernel_size):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     depth = np.array(estimate_depth(image, encoder='vitl'))
-    max_depth = np.max(depth)
-    depth = max_depth - depth
-    depth = depth/max_depth
+    
+    depth = (depth - depth.min()) / (depth.max() - depth.min())
+    depth = 1 - depth
     print("depth estimated")
 
     bokeh_image = bokeh_effect_avg(image, depthmap=depth, focal_distance= focal_distance, kernel_size= kernel_size)
     return bokeh_image
 
 if __name__ == "__main__":
-    image = cv2.imread("assets/images/man.jpg")
-    image = cv2.resize(image, (image.shape[1], image.shape[0]))
-    bokeh_image1 = bokeh_effect(image, focal_distance=0.001, kernel_size=5)
-    bokeh_image2 = bokeh_effect(image, focal_distance=1, kernel_size=5)
+    image = cv2.imread("assets/images/input.jpg")
+    image = cv2.resize(image, (image.shape[1]//4, image.shape[0]//4))
+    bokeh_image1 = bokeh_effect(image, focal_distance=0.1, kernel_size=7)
+    bokeh_image2 = bokeh_effect(image, focal_distance=0.8, kernel_size=7)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     plt.subplot(121)
