8ac6827b5b8ae00ca1262805805e2f5cc656d3b7
SaquibKhan <saquibkhan456@gmail.com>
2024-09-12 20:29:48 +0200

    asset image

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Uses Depth Anything model to simulate depth of field on images
 
 <p align="center">
-  <img src="assets/images/save_img.jpg" alt="Image" />
-  <img src=  "assets/gifs/gif_resize2.gif" alt="GIF"/>
+  <img src="output_image.jpg" alt="Image" />
+
 </p>
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from src.estimate_depth import estimate_depth
-from bokeh import *
+from utils.estimate_depth import estimate_depth
+from utils.bokeh import *
 
 
 def bokeh_effect(image, focal_distance, kernel_size, depth=None):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    if depth==None:
+    if depth is None:
         depth = np.array(estimate_depth(image, encoder='vitl'))
         
         depth = (depth - depth.min()) / (depth.max() - depth.min())
@@ -20,22 +20,24 @@ def bokeh_effect(image, focal_distance, kernel_size, depth=None):
 if __name__ == "__main__":
     image = cv2.imread("assets/images/input.jpg")
     image = cv2.resize(image, (image.shape[1]//4, image.shape[0]//4))
-    bokeh_image1,depth = bokeh_effect(image, focal_distance=0.1, kernel_size=9)
+    bokeh_image1,depth = bokeh_effect(image, focal_distance=0.01, kernel_size=9)
     bokeh_image2, _ = bokeh_effect(image, focal_distance=0.99, kernel_size=9, depth=depth)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
+    
     plt.subplot(141)
     plt.imshow(image)
-    plt.title("Original")
+    plt.title("Original", fontsize=8)
     plt.subplot(142)
     plt.imshow(depth)
-    plt.title("estimated depth")
+    plt.title("estimated depth", fontsize=8)
     plt.subplot(143)
     plt.imshow(bokeh_image1)
-    plt.title("near focus")
+    plt.title("near focus", fontsize=8)
     plt.subplot(144)
     plt.imshow(bokeh_image2)
-    plt.title("far focus")
+    plt.title("far focus", fontsize=8)
+    plt.axis('off')
+    plt.savefig('output_image.jpg', format='jpg', bbox_inches='tight', pad_inches=0)
     plt.show()
 
 
