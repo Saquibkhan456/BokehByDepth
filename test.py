a858da4feaa6d457194e19c64c657470a511fb41
SaquibKhan <saquibkhan456@gmail.com>
2024-09-12 19:38:54 +0200

    depth reuse

--- test.py
+++ test.py
@@ -2,31 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from src.estimate_depth import estimate_depth
-from src.utils import *
+from bokeh import *
 
 
-def bokeh_effect(image, focal_distance, kernel_size):
+def bokeh_effect(image, focal_distance, kernel_size, depth=None):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    depth = np.array(estimate_depth(image, encoder='vitl'))
-    
-    depth = (depth - depth.min()) / (depth.max() - depth.min())
-    depth = 1 - depth
-    print("depth estimated")
+    if depth==None:
+        depth = np.array(estimate_depth(image, encoder='vitl'))
+        
+        depth = (depth - depth.min()) / (depth.max() - depth.min())
+        depth = 1 - depth
+        print("depth estimated")
 
     bokeh_image = bokeh_effect_avg(image, depthmap=depth, focal_distance= focal_distance, kernel_size= kernel_size)
-    return bokeh_image
+    return bokeh_image, depth
 
 if __name__ == "__main__":
     image = cv2.imread("assets/images/input.jpg")
     image = cv2.resize(image, (image.shape[1]//4, image.shape[0]//4))
-    bokeh_image1 = bokeh_effect(image, focal_distance=0.1, kernel_size=7)
-    bokeh_image2 = bokeh_effect(image, focal_distance=0.8, kernel_size=7)
+    bokeh_image1,depth = bokeh_effect(image, focal_distance=0.1, kernel_size=9)
+    bokeh_image2, _ = bokeh_effect(image, focal_distance=0.99, kernel_size=9, depth=depth)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    plt.subplot(121)
+    plt.subplot(141)
+    plt.imshow(image)
+    plt.title("Original")
+    plt.subplot(142)
+    plt.imshow(depth)
+    plt.title("estimated depth")
+    plt.subplot(143)
     plt.imshow(bokeh_image1)
     plt.title("near focus")
-    plt.subplot(122)
+    plt.subplot(144)
     plt.imshow(bokeh_image2)
     plt.title("far focus")
     plt.show()
